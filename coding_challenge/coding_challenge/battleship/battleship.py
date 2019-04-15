89f353b968123474be646f8de9932315784b0c32
oli <richtero@ethz.ch>
2019-04-15 14:26:04 +0200

    Adds 'ship sunk' info

--- coding_challenge/coding_challenge/battleship/battleship.py
+++ coding_challenge/coding_challenge/battleship/battleship.py
@@ -9,11 +9,14 @@ class BattleshipEnv(gym.Env):
         self.action_space = spaces.Box(0.0, 1.0, (2,))
         self.observation_space = spaces.Box(0.0, 1.0, (10, 10, 1))
 
+    def _outside(self, x_idx, y_idx):
+        return x_idx < 0 or x_idx >= 10 or y_idx < 0 or y_idx >= 10
+
     def _check_space(self, vertical, start_x, start_y, size):
         for x_idx_delta in range(3):
             for y_idx in range(start_y - 1, start_y + size + 1):
                 x_idx = start_x - 1 + x_idx_delta
-                if x_idx < 0 or x_idx >= 10 or y_idx < 0 or y_idx >= 10:
+                if self._outside(x_idx, y_idx):
                     continue
                 elif vertical and self._ships[x_idx][y_idx] > 0.0:
                     return False
@@ -33,6 +36,36 @@ class BattleshipEnv(gym.Env):
                 self._ships[x_idx][y_idx] = 1.0
             else:
                 self._ships[y_idx][x_idx] = 1.0
+
+    def _check_ship_sunk(self, x_idx, y_idx):
+        places_to_update = [(x_idx, y_idx)]
+        for delta_x in range(1, 4):
+            if self._outside(x_idx + delta_x, y_idx) or self._ships[x_idx + delta_x][y_idx] == 0.0:
+                break
+            elif self._ships[x_idx + delta_x][y_idx] == 1.0:
+                return False
+            places_to_update.append((x_idx + delta_x, y_idx))
+        for delta_x in range(1, 4):
+            if self._outside(x_idx - delta_x, y_idx) or self._ships[x_idx - delta_x][y_idx] == 0.0:
+                break
+            elif self._ships[x_idx - delta_x][y_idx] == 1.0:
+                return False
+            places_to_update.append((x_idx - delta_x, y_idx))
+        for delta_y in range(1, 4):
+            if self._outside(x_idx, y_idx + delta_y) or self._ships[x_idx][y_idx + delta_y] == 0.0:
+                break
+            elif self._ships[x_idx][y_idx + delta_y] == 1.0:
+                return False
+            places_to_update.append((x_idx, y_idx + delta_y))
+        for delta_y in range(1, 4):
+            if self._outside(x_idx, y_idx - delta_y) or self._ships[x_idx][y_idx - delta_y] == 0.0:
+                break
+            elif self._ships[x_idx][y_idx - delta_y] == 1.0:
+                return False
+            places_to_update.append((x_idx, y_idx - delta_y))
+        for x_y in places_to_update:
+            self._board[x_y[0]][x_y[1]] = 1.0
+        return True
 
     def _place_ships(self):
         self._ships = np.zeros(shape=(10, 10, 1))
@@ -56,7 +89,7 @@ class BattleshipEnv(gym.Env):
         y_idx = int(action[1] * 10)
         terminal = False
         self._step += 1
-        if self._step > self.max_steps:
+        if self._step >= self.max_steps:
             info = {'game_message': 'You loose!'}
             reward = 0.0
             terminal = True
@@ -65,15 +98,17 @@ class BattleshipEnv(gym.Env):
             reward = -1.0
         elif self._ships[x_idx][y_idx] > 0.0:
             info = {'game_message': 'Hit'}
-            self._board[x_idx][y_idx] = 1.0
-            self._ships[x_idx][y_idx] = 0.0
-            if np.sum(self._ships) == 0.0:
-                info = {'game_message': 'You win!'}
-                terminal = True
+            self._board[x_idx][y_idx] = 0.6
+            self._ships[x_idx][y_idx] = -1.0
+            if self._check_ship_sunk(x_idx, y_idx):
+                info = {'game_message': 'Ship sunk!'}
+                if np.sum(self._ships) == -(1*4.0 + 2*3.0 + 3*2.0 + 4*1.0):
+                    info = {'game_message': 'You win!'}
+                    terminal = True
             reward = 1.0
         else:
             info = {'game_message': 'Miss'}
             reward = -0.1
             if self._board[x_idx][y_idx] == 0.0:
-                self._board[x_idx][y_idx] = 0.5
+                self._board[x_idx][y_idx] = 0.3
         return self._board, reward, terminal, info
